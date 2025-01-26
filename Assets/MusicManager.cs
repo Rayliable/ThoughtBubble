bd54c7e97ee9317583f47546ceec3026c1132a05
LayZNimrod <2zachmiller@gmail.com>
2025-01-26 14:19:37 -0500

    Made Music Work

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] AudioSource Menu;
+    [SerializeField] AudioSource Test;
+    [SerializeField] AudioSource Water;
+    [SerializeField] AudioSource Tech;
+    [SerializeField] AudioSource Spy;
+    [SerializeField] AudioSource Eppy;
+    
+    public enum Music {None, Menu, Test, Water, Tech, Spy, Eppy};
+    private Music playing = Music.None;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject); ;
+    }
+
+    // Update is called once per frame
+    public void PlayAudio(Music Type)
+    {
+        switch (Type)
+        {
+            case Music.Menu:
+                if (playing != Music.Menu)
+                {
+                    Menu.Play();
+                    playing = Music.Menu;
+                }
+                break;
+
+            case Music.Test:
+                if (playing != Music.Test)
+                {
+                    Test.Play();
+                    playing = Music.Test;
+                }
+                break;
+
+            case Music.Water:
+                if (playing != Music.Water)
+                {
+                Water.Play();
+                playing = Music.Water;
+                }
+                break;
+
+            case Music.Tech:
+                if (playing != Music.Tech)
+                {
+                    Tech.Play();
+                    playing = Music.Tech;
+                }
+                break;
+
+            case Music.Spy:
+                if (playing != Music.Spy)
+                {
+                    Spy.Play();
+                    playing = Music.Spy;
+                }
+                break;
+
+            case Music.Eppy:
+                if (playing != Music.Eppy)
+                {
+                    Eppy.Play();
+                    playing = Music.Menu;
+                }
+                break;
+
+            default:
+                playing = Music.None;
+                break;
+        }
+    }
+}
